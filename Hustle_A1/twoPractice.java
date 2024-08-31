3e2c67d973049d86ee0df2d2560cb611b1b4e4c6
Sumit6306 <sumitpandey67890@gmail.com>
2024-08-31 23:57:34 +0530

    Unique value

--- Hustle_A1/twoPractice.java
+++ Hustle_A1/twoPractice.java
@@ -5,5 +5,30 @@ import java.util.Scanner;
 public class twoPractice {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Unique value is :" + uniqueValue(arr));
+
+    }
+    static int uniqueValue(int[] arr) {
+        int ans = -1;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i]==arr[j]) {
+                    arr[i]=-1;
+                    arr[j]=-1;
+                }
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]>0) {
+                ans=arr[i];
+            }
+        }
+        return ans;
     }
 }
