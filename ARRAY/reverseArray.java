cd033925c5ece1710df7bd459eed1f29123ed15d
Sumit6306 <yng.sumit@gmail.com>
2024-09-07 08:41:07 +0530

    Two Pointer Reverse

--- ARRAY/reverseArray.java
+++ ARRAY/reverseArray.java
@@ -1,11 +1,47 @@
 package DSA.ARRAY;
 
+import java.util.Scanner;
+
 public class reverseArray {
-static int [] reverse (int [] arr) {
-    int n = arr.length;
-int m =10;
+static void reverse (int [] arr) {
+      for (int i = 0; i < arr.length / 2; i++) {
+              int temp = arr[i];
+              arr[i] = arr[arr.length-1-i];
+             arr[arr.length-1-i]= temp;
+          }
+      }
+      static void tpReverse (int [] arr) {
+    int i=0;
+    int j=arr.length-1;
+  while(i<=j) {
+      int temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+      i++;
+      j--;
+  }
+}
 
 
-    return arr;
+static void printArray(int[]arr){
+    for(int i = 0; i< arr.length;i++){
+        System.out.print(arr[i]+" ");
+    }
+    System.out.println();
+}
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the size of the array");
+    int n = sc.nextInt();
+    int [] arr = new int[n];
+
+    System.out.println("Enter the elements of the array");
+    for (int i = 0; i < arr.length; i++) {
+        arr[i] = sc.nextInt();
+    }
+    tpReverse(arr);
+    System.out.println("Reversed Array");
+   printArray(arr);
+
 }
 }
