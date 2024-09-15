b5d33d81568b3e076b79038c543d815c8caaf49f
Sumit6306 <yng.sumit@gmail.com>
2024-09-15 23:23:36 +0530

    Spiral  Pattern Question

---
+++ MD_Array/TwoD_ArrayList/functionArrayList.java
@@ -0,0 +1,10 @@
+package DSA.MD_Array.TwoD_ArrayList;
+import java.util.*;
+public class functionArrayList {
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();//One-D Array list
+
+
+    }
+}
