981e904fa8e951feab8ef0b4c582687d9f83eb7a
Sumit6306 <yng.sumit@gmail.com>
2024-09-16 23:17:57 +0530

    pascal  Pattern Question

---
+++ MD_Array/TwoD_ArrayList/pascalTriangleQuestion.java
@@ -0,0 +1,16 @@
+package DSA.MD_Array.TwoD_ArrayList;
+import java.util.*;
+class functionArrayList {
+   static void pascalTriangle(int [][] arr) {
+        List<Integer> list = new ArrayList<>();//One-D Array list
+for (int i = 1; i <=i ; i++) {
+
+}
+
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+}
